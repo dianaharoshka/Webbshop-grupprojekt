bf4334f6c90e90d37eeb5f3501eb8901902ed7a2
SamuelSandevik <samuel.sandevik@outlook.com>
2024-12-27 11:13:40 +0100

    Added Api call to dummyjson for products

---
+++ src/services/DummyJSONCall.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { Product } from "../types/IProduct";
+
+async function fetchProducts(): Promise<Product[]> {
+  try {
+    const response = await axios.get<{ products: Product[] }>(
+      "https://dummyjson.com/products"
+    );
+    return response.data.products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw new Error("Failed to fetch products.");
+  }
+}
